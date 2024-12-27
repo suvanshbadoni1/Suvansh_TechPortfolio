701aa1ff130053583fec643368954e68c453ecd4
Suvansh badoni <suvanshbadoni@gmail.com>
2024-12-27 18:52:45 +0530

    update error

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $(".client_owl-carousel").owlCarousel({
 document.querySelector('a[href="#info"]').addEventListener('click', function (e) {
     e.preventDefault();
     document.getElementById('info').scrollIntoView({ behavior: 'smooth' });
-  });
+});
 
 
 
@@ -45,8 +45,48 @@ document.querySelector('a[href="#info"]').addEventListener('click', function (e)
 function redirectToTop() {
     // Wait for a brief moment to ensure the form submission completes
     setTimeout(() => {
-      // Redirect back to the same page
-      window.location.href = window.location.pathname; // Reloads the current page from the top
+        // Redirect back to the same page
+        window.location.href = window.location.pathname; // Reloads the current page from the top
     }, 3000); // Adjust delay as needed for form processing
     return true; // Allow the form submission to proceed
-  }
+}
+
+
+//Handle SUbmit
+async function handleFormSubmit(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    const form = document.getElementById('myForm');
+    const formData = new FormData(form);
+    const messageDiv = document.getElementById('message');
+
+    try {
+        const response = await fetch(form.action, {
+            method: form.method,
+            body: formData,
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok && responseData.result === 'success') {
+            messageDiv.style.color = 'green';
+            messageDiv.textContent = 'Thank you for subscribing!';
+        } else {
+            throw new Error(responseData.error || 'An error occurred. Please try again.');
+        }
+    } catch (error) {
+        // messageDiv.style.color = 'red';
+        // messageDiv.textContent = error.message;
+        console.log(error);
+
+    }
+
+    messageDiv.style.display = 'block';
+
+    // Wait 3 seconds and then reload the page
+    setTimeout(() => {
+        window.location.href = window.location.pathname; // Reload the current page
+    }, 2000);
+
+    return true; // Allow the form processing to complete
+}
